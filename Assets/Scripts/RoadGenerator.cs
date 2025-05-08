dfb99bb351c082e3ec74710ead6fa4b807e01561
gvidas <danieliusgvidas@gmail.com>
2025-05-08 12:04:32 +0100

    implemented basic propogation

--- Assets/Scripts/RoadGenerator.cs
+++ Assets/Scripts/RoadGenerator.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using UnityEditor.TerrainTools;
 using UnityEngine;
 
 public class HeadNode {
     public Vector3 pos;
     public Vector3 dir;
+    public Node prevNode;
 
     public HeadNode(Vector3 p, Vector3 d) {
         pos = p;
@@ -13,6 +15,7 @@ public class HeadNode {
 
 public class Node {
     public Vector3 pos;
+    public List<Node> connections = new List<Node>();
 
     public Node(Vector3 p) {
         pos = p;
@@ -23,7 +26,7 @@ public class RoadGenerator : MonoBehaviour {
 
     [SerializeField] private GameObject testBall;
 
-    private HeadNode headNode;
+    private List<HeadNode> headNodes = new List<HeadNode>();
     private List<Node> nodes = new List<Node>();
 
     private float lastTicked;
@@ -33,7 +36,7 @@ public class RoadGenerator : MonoBehaviour {
     void Start()
     {
         lastTicked = Time.time;
-        headNode = new HeadNode(Vector3.zero, Vector3.forward);
+        headNodes.Add(new HeadNode(Vector3.zero, Vector3.forward));
     }
 
     // Update is called once per frame
@@ -46,14 +49,54 @@ public class RoadGenerator : MonoBehaviour {
     }
     
     void IterateGraph() {
-        Node n = new Node(headNode.pos);
+        for (int i = 0; i < headNodes.Count; i++) {
+            HeadNode h = headNodes[i];
+            
+            if (Random.Range(0,5) == 1) {
+                SplitHeadNode(h);
+            }
+            
+            IterateHead(h);
+        }
+        DrawConnections();
+    }
+    
+    void IterateHead(HeadNode h) {
+        Node n = new Node(h.pos);
+        if (h.prevNode != null) {
+            n.connections.Add(h.prevNode);
+
+        }
+        h.prevNode?.connections.Add(n);
         nodes.Add(n);
         DisplayNode(n);
-        headNode.pos += headNode.dir * 10;
+        h.pos += h.dir * 10;
+        h.prevNode = n;
     }
     
     void DisplayNode(Node n) {
         Instantiate(testBall, n.pos, Quaternion.identity);
+    }
+    
+    void DrawConnections() {
+        for (int i = 0; i < nodes.Count; i++) {
+            Node node = nodes[i];
+
+            for (int j = 0; j < node.connections.Count; j++) {
+                Node neighbour = node.connections[j];
+                Vector3 pos1 = node.pos;
+                Vector3 pos2 = neighbour.pos;
+                Debug.DrawLine(pos1, pos2, Color.red, 1.0f);
+            }
+        }
+    }
+
+    
+    void SplitHeadNode(HeadNode h) {
+        Vector3 newDir = Quaternion.AngleAxis(45f, Vector3.up) * h.dir;
+        HeadNode newHead = new HeadNode(h.prevNode.pos + newDir * 10, newDir);
+        newHead.prevNode = h.prevNode;
+        headNodes.Add(newHead);
     }
     
 }
